e8d4fa63293de1faaacb670d9b38470776159f92
eonsubae <eonsubae@naver.com>
2021-02-11 23:35:37 +0900

    start

---
+++ StartSUI/StartSUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StartSUI
+//
+//  Created by KRITSSEAN on 2021/02/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, SwiftUI!")
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+            .fontWeight(/*@START_MENU_TOKEN@*/.heavy/*@END_MENU_TOKEN@*/)
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
